6cc31b23ea0739916538ba318e78741fc726c141
Mykhailo418 <smmichael418@gmail.com>
2019-10-11 20:28:51 +0300

    paddle, ball hit paddle collision

--- ball/script.js
+++ ball/script.js
@@ -4,42 +4,95 @@ window.onload = function(){
 
   // Ball Params
   const radius = 10;
-  let speedX = 2;
-  let speedY = 1;
-  let ballX = 100;
-  let ballY = 100;
+  const initSpeedX = 4;
+  const initSpeedY = 3;
+  let speedX = initSpeedX;
+  let speedY = initSpeedY;
+  let ballX = canvas.width/2;
+  let ballY = canvas.height/2;
 
-  animation(update)
+  // Paddle Params
+  const paddleWidth = 100;
+  const paddleHeight = 10;
+  let paddleX = canvas.width/2 - paddleWidth/2;
+  let paddleY = canvas.height - paddleHeight - 20;
 
-  // Update
+  animation(update);
+  window.addEventListener('mousemove', updateMouseMove);
+
+  // ---
+
   function update(){
+    moveAll();
+    drawAll();
+  }
+
+  function moveAll(){
     ballX += speedX;
     ballY += speedY;
 
     // Change direction
     if (ballX + radius > canvas.width || ballX - radius < 0) speedX *= -1;
-    if (ballY + radius > canvas.height || ballY - radius < 0) speedY *= -1;
+    if (ballY - radius < 0) speedY *= -1;
+    if (ballY + radius > canvas.height) reset();
 
-    // Black rectangle
-    ctx.fillStyle = 'black';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    let paddleTopEdgeY = paddleY;
+    let paddleBottomEdgeY = paddleY + paddleHeight;
+    let paddleLeftEdgeX = paddleX;
+    let paddleRightEdgeX = paddleX + paddleWidth;
 
-    // Draw Ball
-    ctx.fillStyle = 'white';
+    if (paddleTopEdgeY <= ballY + radius &&
+        paddleBottomEdgeY >= ballY + radius &&
+        paddleLeftEdgeX <= ballX + radius &&
+        paddleRightEdgeX >= ballX - radius)
+    {
+       speedY = -Math.abs(speedY);
+    }
+  }
+
+  function reset(){
+    ballX = canvas.width/2;
+    ballY = canvas.height/2;
+    speedY = Math.abs(speedY);
+  }
+
+  function drawAll(){
+    // background
+    drawRect(0, 0, canvas.width, canvas.height, 'black');
+    // ball
+    drawBall(ballX, ballY, radius, 'white');
+    //paddle
+    drawRect(paddleX, paddleY, paddleWidth, paddleHeight, 'white');
+  }
+
+  function drawRect(top, left, width, height, color){
+    ctx.fillStyle = color;
+    ctx.fillRect(top, left, width, height);
+  }
+
+  function drawBall(ballX, ballY, radius, color){
+    ctx.fillStyle = color;
     ctx.beginPath();
     ctx.arc(ballX, ballY, radius, 0, Math.PI*2, true);
     ctx.fill();
   }
 
-  // Animation
   function animation(callback){
     let start = null;
+    let requestID = window.requestAnimationFrame(step);
     function step(timestamp) {
       if (!start) start = timestamp;
       let progress = timestamp - start;
       callback();
       requestID = window.requestAnimationFrame(step);
     }
-    let requestID = window.requestAnimationFrame(step)
+  }
+
+  function updateMouseMove(e){
+    const rect = canvas.getBoundingClientRect();
+    let mouseX = e.clientX - rect.left - pageXOffset
+    //let mouseY = e.clientY - rect.top - pageYOffset
+
+    paddleX = mouseX - paddleWidth/2;
   }
 }
