bb44ce3f379ab78e3fa3b2a7e58190976fcd2df5
Mykhailo418 <smmichael418@gmail.com>
2019-10-11 13:43:11 +0300

    animation move of ball, change direction after hitting borders of canvas

--- ball/script.js
+++ ball/script.js
@@ -2,11 +2,44 @@ window.onload = function(){
   const canvas = document.getElementById('canvas')
   const ctx = canvas.getContext('2d');
 
-  ctx.fillStyle = 'black';
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  // Ball Params
+  const radius = 10;
+  let speedX = 2;
+  let speedY = 1;
+  let ballX = 100;
+  let ballY = 100;
 
-  ctx.fillStyle = 'white';
-  ctx.beginPath();
-  ctx.arc(100, 100, 10, 0, Math.PI*2, true);
-  ctx.fill();
+  animation(update)
+
+  // Update
+  function update(){
+    ballX += speedX;
+    ballY += speedY;
+
+    // Change direction
+    if (ballX + radius > canvas.width || ballX - radius < 0) speedX *= -1;
+    if (ballY + radius > canvas.height || ballY - radius < 0) speedY *= -1;
+
+    // Black rectangle
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    // Draw Ball
+    ctx.fillStyle = 'white';
+    ctx.beginPath();
+    ctx.arc(ballX, ballY, radius, 0, Math.PI*2, true);
+    ctx.fill();
+  }
+
+  // Animation
+  function animation(callback){
+    let start = null;
+    function step(timestamp) {
+      if (!start) start = timestamp;
+      let progress = timestamp - start;
+      callback();
+      requestID = window.requestAnimationFrame(step);
+    }
+    let requestID = window.requestAnimationFrame(step)
+  }
 }
