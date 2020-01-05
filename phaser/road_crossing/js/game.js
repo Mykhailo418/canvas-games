68a4a987b501989d71074511ebfa4e943753216a
Mykhailo418 <smmichael418@gmail.com>
2020-01-05 19:41:35 +0200

    Road Crossing: Update State, add moving of player, add gold sprite

--- phaser/road_crossing/js/game.js
+++ phaser/road_crossing/js/game.js
@@ -3,23 +3,40 @@ const ASSESTS = {
   background: 'background',
   player: 'player',
   enemy: 'enemy',
+  gold: 'gold',
 }
 
 // Create new scene
 const gameScene = new Phaser.Scene('Game');
+
+// Initial Params
+gameScene.init =  function() {
+  this.playerSpeed = 2;
+}
 
 // Load Assets
 gameScene.preload = function() {
   this.load.image(ASSESTS.background, 'img/bg.jpg');
   this.load.image(ASSESTS.player, 'img/player.png');
   this.load.image(ASSESTS.enemy, 'img/monster.png');
+  this.load.image(ASSESTS.gold, 'img/gold.png');
 }
 
 // Called once after load
 gameScene.create = function() {
   addBackground(this);
-  addPlayer(this);
-  addEnemy(this);
+  this.player = addPlayer(this);
+  this.enemy = addEnemy(this);
+  this.gold = addGold(this);
+}
+
+// Called upto 60 times per second
+gameScene.update = function() {
+  const {width} = this.sys.game.config;
+  // check for active input
+  if (this.input.activePointer.isDown) {
+    this.player.x = Math.min(this.player.x + this.playerSpeed, width - this.player.displayWidth/2);
+  }
 }
 
 // set config
@@ -45,6 +62,8 @@ function addPlayer(scene) {
   const player = scene.add.sprite(50, 250, ASSESTS.player)
     .setScale(0.05);
   player.depth = 1; // like z-index
+  //player.setAngle(45); // rotation
+  return player;
 }
 
 function addEnemy(scene) {
@@ -53,4 +72,14 @@ function addEnemy(scene) {
   enemy.scaleY = 0.2;
   enemy.flipX = true;
   enemy.depth = 1;
+  return enemy;
+}
+
+function addGold(scene) {
+  const {width} = scene.sys.game.config;
+  const gold = scene.add.sprite(0, 0, ASSESTS.gold);
+  gold.setScale(0.06);
+  gold.setPosition(width - gold.displayWidth/2 - 5, 270);
+  gold.depth = 1;
+  return gold;
 }
