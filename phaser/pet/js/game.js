dbffb4ddc98be6a804905fabf987c496e4eae624
Mykhailo418 <smmichael418@gmail.com>
2020-01-13 21:07:56 +0200

    Pet: setup

---
+++ phaser/pet/js/game.js
@@ -0,0 +1,28 @@
+
+const gameScene = new Phaser.Scene('Game');
+
+gameScene.init = function() {
+
+};
+
+gameScene.preload = function() {
+
+};
+
+gameScene.create = function() {
+
+};
+
+gameScene.update = function() {
+
+};
+
+const game = new Phaser.Game({
+  type: Phaser.AUTO,
+  width: 360,
+  height: 640,
+  scene: gameScene,
+  title: 'Virtual Pet',
+  pixelArt: false,
+  backgroundColor: 'ffffff'
+});
