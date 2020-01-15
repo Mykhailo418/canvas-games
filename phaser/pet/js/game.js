bf36bbe30106d71be872d7b0d8858f901552f5ac
Mykhailo418 <smmichael418@gmail.com>
2020-01-15 21:51:47 +0200

    Pet: add spritesheet of pet, add sprites of buttons

--- phaser/pet/js/game.js
+++ phaser/pet/js/game.js
@@ -1,11 +1,15 @@
 const ASSESTS = {
-  toy: 'toy', apple: 'apple', bg: 'background', candy: 'candy', rotate: 'rotate'
-}
+  toy: 'toy', apple: 'apple', bg: 'background', candy: 'candy', rotate: 'rotate', pet: 'pet'
+};
+const globals = {};
 
 const gameScene = new Phaser.Scene('Game');
 
 gameScene.init = function() {
-
+  globals.state = {
+    health: 100,
+    fun: 100
+  }
 };
 
 gameScene.preload = function() {
@@ -14,10 +18,22 @@ gameScene.preload = function() {
   this.load.image(ASSESTS.apple, 'img/apple.png');
   this.load.image(ASSESTS.candy, 'img/candy.png');
   this.load.image(ASSESTS.rotate, 'img/rotate.png');
+
+  this.load.spritesheet(ASSESTS.pet, 'img/pet.png', {
+    frameWidth: 273,
+    frameHeight: 200,
+    spacing: 1
+  });
 };
 
 gameScene.create = function() {
   this.add.image(0, 0, ASSESTS.bg).setOrigin(0, 0);
+
+  globals.pet = this.add.sprite(100, 200, ASSESTS.pet, 0)
+    .setOrigin(0)
+    .setScale(0.5);
+
+  addButtons(this);
 };
 
 gameScene.update = function() {
@@ -33,3 +49,18 @@ const game = new Phaser.Game({
   pixelArt: false,
   backgroundColor: 'ffffff'
 });
+
+function addButtons(scene) {
+  globals.appleBtn = scene.add.sprite(42, 580, ASSESTS.apple)
+    .setScale(0.4)
+    .setInteractive();
+  globals.candyBtn = scene.add.sprite(134, 580, ASSESTS.candy)
+    .setScale(0.45)
+    .setInteractive();
+  globals.toyBtn = scene.add.sprite(226, 580, ASSESTS.toy)
+    .setScale(0.45)
+    .setInteractive();
+  globals.rotateBtn = scene.add.sprite(318, 580, ASSESTS.rotate)
+    .setScale(0.4)
+  . setInteractive();
+}
